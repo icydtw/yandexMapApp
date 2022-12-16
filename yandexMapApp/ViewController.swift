8700afb1a9d07f0847e42f03e8524002197c0fdd
icydtw <a@b-c-d.ru>
2022-12-16 08:34:09 +0500

    Initial Commit

---
+++ yandexMapApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  yandexMapApp
+//
+//  Created by Илья Тимченко on 16.12.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
